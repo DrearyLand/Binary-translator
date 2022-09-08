ef5a87090122777f036859f698072d9016e8bdb3
DrearyAcademic <111543820+DrearyAcademic@users.noreply.github.com>
2022-09-08 08:00:50 -0500

    Decimal a binario activo
    Se creo la función para la traducción de decimal a binario, ya esta funcional.

--- Avance_1.py
+++ Avance_1.py
@@ -1,24 +1,31 @@
-def binario_a_texto():
-    binario = int(input("Ingrese su codigo binario:"))
-    print("Aqui se mostrara tu traduccion.")
-def texto_a_binario():
+def binario_a_decimal(num):
+    potencia =1
+    decimal= 0
+    while num>0:
+        binario = num % 10
+        num = int(num / 10)
+        decimal = decimal + binario * potencia
+        potencia = potencia * 2
+    return decimal
+def decimal_a_binario():
     binario = input("Ingrese su texto:")
     print("Aqui se mostrara tu traduccion.")
 def main():
     menu()
     entrada = int(input(":"))
     if entrada==1:
-        binario_a_texto()
+        num = int(input("Ingrese el numero binario: "))
+        print("Su valor decimal es =", binario_a_decimal(num))
     elif entrada==2:
-        texto_a_binario()
+        decimal_a_binario()
     elif entrada==3:
         print("Adios")
     else:
         print("Ingrese un valor valido la proxima vez.")
 
 def menu():
-    print("Ingrese 1 si desea traducir codigo binario a texto.")
-    print("Ingrese 2 si desea traducir texto a codigo binario. ")
+    print("Ingrese 1 si desea traducir codigo binario a decimal.")
+    print("Ingrese 2 si desea traducir decimal a codigo binario. ")
     print("Ingrese 3 si quiere salir.")
 
 main()
